452786a13d9ad7376077074251a60e48ec038418
Pachipirata <145575699+Pachipirata@users.noreply.github.com>
2023-10-09 22:16:52 -0300

    Add files via upload

---
+++ proyecto 2/js/script.js
@@ -0,0 +1,27 @@
+//variable numerica
+numero = 1
+console.log (numero)
+//variable string dentro de un comentario multilinea
+saludo= "hola"
+console.log (saludo);
+
+nombre= "Andres"
+apellido="Llanes"
+
+console.log("bienvenido", nombre, apellido)
+
+document.write("Bienvenido", nombre," ",apellido)
+
+var1= 450//variable tipo numerico
+
+total= var1 + numero;
+document.write (var1)
+
+var3= date();
+
+document.write (var3)
+
+console.log (total)
+
+var2= "Juanito"//cadena de caracteres- string va entre comillas
+document.write (var2)
