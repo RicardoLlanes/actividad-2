6022b9a3a81675176578feedc729eb969ebda505
Pachipirata <145575699+Pachipirata@users.noreply.github.com>
2023-10-18 03:14:57 -0300

    cambios agregados en proyecto 2
    modificacion del archivo contacto y alineacion de las imagenes

--- proyecto 2/js/script.js
+++ proyecto 2/js/script.js
@@ -8,20 +8,20 @@ console.log (saludo);
 nombre= "Andres"
 apellido="Llanes"
 
-console.log("bienvenido", nombre, apellido)
+//console.log("bienvenido", nombre, apellido)
 
-document.write("Bienvenido", nombre," ",apellido)
+//document.write("Bienvenido", nombre," ",apellido)
 
 var1= 450//variable tipo numerico
 
 total= var1 + numero;
-document.write (var1)
+//document.write (var1)
 
 var3= date();
 
 document.write (var3)
 
-console.log (total)
+//console.log (total)
 
 var2= "Juanito"//cadena de caracteres- string va entre comillas
-document.write (var2)
+//document.write (var2)
